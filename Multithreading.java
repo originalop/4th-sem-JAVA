f3759fa6e813f04328caecb0495707622d07a3aa
originalop <163742860+originalop@users.noreply.github.com>
2025-03-06 15:45:03 +0530

    Add files via upload

---
+++ Multithreading.java
@@ -0,0 +1,19 @@
+class ForMyJob extends Thread {
+
+    void square(int num) {
+        System.out.println("Square is: " + num * num);
+    }
+
+    @Override
+    public void run() {
+        square(6);
+    }
+
+}
+
+public class Multithreading {
+    public static void main(String[] args) {
+        ForMyJob t1 = new ForMyJob();
+        t1.start();
+    }
+}
